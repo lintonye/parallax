ad49e5d0da8263800320b2fe18caa077631d6910
Linton Ye <linton@jimulabs.com>
2019-05-19 15:16:01 -0700

    Remove animate from iPhone demo

--- code/iPhoneXRScrolling.tsx
+++ code/iPhoneXRScrolling.tsx
@@ -1,9 +1,9 @@
-import { Data, animate, Override, Animatable, transform } from "framer";
-import { scrollOverrides, modulate, speedY, stickyY } from "./ScrollOverrides";
+import { Data, Override } from "framer"
+import { scrollOverrides, modulate, speedY, stickyY } from "./ScrollOverrides"
 
 const data = Data({
-  phoneNameSizeOpacity: Animatable(0)
-});
+  phoneNameSizeOpacity: 0
+})
 
 const overrides = scrollOverrides(
   [0, 200],
@@ -35,10 +35,8 @@ const overrides = scrollOverrides(
   [750, 800],
   [
     {
-      op: getData => ({ vy }) => {
-        animate.ease(data.phoneNameSizeOpacity, vy > 0 ? 1 : 0, {
-          duration: 0.2
-        });
+      op: () => ({ vy }) => {
+        data.phoneNameSizeOpacity = vy > 0 ? 1 : 0
       }
     }
   ],
@@ -54,22 +52,22 @@ const overrides = scrollOverrides(
   [{ id: "feature4", op: modulate("opacity", [0, 1]) }],
   [1000, 1050],
   [{ id: "feature5", op: modulate("opacity", [0, 1]) }]
-);
+)
 
-export const Scroll: Override = props => overrides.scroll(props);
-export const Blocker: Override = props => overrides.blocker(props);
-export const IPhoneXR: Override = props => overrides.iPhoneXR(props);
+export const Scroll: Override = props => overrides.scroll(props)
+export const Blocker: Override = props => overrides.blocker(props)
+export const IPhoneXR: Override = props => overrides.iPhoneXR(props)
 
 export const PhoneNameSize: Override = props => ({
   ...overrides.phoneNameSize(props),
-  opacity: data.phoneNameSizeOpacity
-});
+  animate: { opacity: data.phoneNameSizeOpacity }
+})
 
 export const FeatureContainer: Override = props =>
-  overrides.featureContainer(props);
+  overrides.featureContainer(props)
 
-export const Feature1: Override = props => overrides.feature1(props);
-export const Feature2: Override = props => overrides.feature2(props);
-export const Feature3: Override = props => overrides.feature3(props);
-export const Feature4: Override = props => overrides.feature4(props);
-export const Feature5: Override = props => overrides.feature5(props);
+export const Feature1: Override = props => overrides.feature1(props)
+export const Feature2: Override = props => overrides.feature2(props)
+export const Feature3: Override = props => overrides.feature3(props)
+export const Feature4: Override = props => overrides.feature4(props)
+export const Feature5: Override = props => overrides.feature5(props)
