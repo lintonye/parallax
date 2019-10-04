347b45b29f423eac3677d008ca873a5e16ea5bc2
Linton Ye <linton@jimulabs.com>
2019-10-04 06:08:25 -0700

    Add thumbnails

--- code/ParallaxLayer.tsx
+++ code/ParallaxLayer.tsx
@@ -1,164 +1,193 @@
 import * as React from "react"
 import {
-    Frame,
-    addPropertyControls,
-    ControlType,
-    useTransform,
-    RenderTarget,
+  Frame,
+  addPropertyControls,
+  ControlType,
+  useTransform,
+  RenderTarget
 } from "framer"
 import { ScrollContext } from "./ScrollContext"
+import { EmptyConnector } from "./EmptyConnector"
 
 function ParallaxLayerPreview({
-    children,
-    inputMin,
-    inputMax,
-    property,
-    dependsOn,
-    ...props
+  children,
+  inputMin,
+  inputMax,
+  property,
+  dependsOn,
+  ...props
 }) {
-    const { scrollX, scrollY } = React.useContext(ScrollContext)
-    const outputMin = props[`${property}_outputMin`]
-    const outputMax = props[`${property}_outputMax`]
-    const value = useTransform(
-        dependsOn === "y" ? scrollY : scrollX,
-        [inputMin, inputMax],
-        [outputMin, outputMax]
-    )
-    const transformedProps = { [property]: value }
-    return (
-        <Frame background={null} {...transformedProps} {...props}>
-            {children}
-        </Frame>
-    )
+  const { scrollX, scrollY } = React.useContext(ScrollContext)
+  const outputMin = props[`${property}_outputMin`]
+  const outputMax = props[`${property}_outputMax`]
+  const value = useTransform(
+    dependsOn === "y" ? scrollY : scrollX,
+    [inputMin, inputMax],
+    [outputMin, outputMax]
+  )
+  const transformedProps = { [property]: value }
+  return (
+    <Frame background={null} {...transformedProps} {...props}>
+      {children}
+    </Frame>
+  )
 }
 
 function ParallaxLayerCanvas(props) {
-    return <Frame background={null} {...props} />
+  return React.Children.count(props.children) > 0 ? (
+    <Frame background={null} {...props} />
+  ) : (
+    <EmptyConnector title="Connect to a Frame →" description="" />
+  )
+}
+
+function ParallaxLayerThumbnail(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="200"
+      height="200"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-file"
+      viewBox="0 0 24 24"
+    >
+      <path d="M13 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V9z" />
+      <path d="M13 2L13 9 20 9" />
+    </svg>
+  )
 }
 
 export function ParallaxLayer(props) {
-    const Comp =
-        RenderTarget.current() === RenderTarget.preview
-            ? ParallaxLayerPreview
-            : ParallaxLayerCanvas
-    return <Comp {...props} />
+  const target = RenderTarget.current()
+  const Comp =
+    target === RenderTarget.preview
+      ? ParallaxLayerPreview
+      : target === RenderTarget.thumbnail
+      ? ParallaxLayerThumbnail
+      : ParallaxLayerCanvas
+  return <Comp {...props} />
 }
 
 const properties = {
-    x: {
-        x_outputMin: {
-            type: ControlType.Number,
-            title: "X Min",
-        },
-        x_outputMax: {
-            type: ControlType.Number,
-            title: "X Max",
-        },
+  x: {
+    x_outputMin: {
+      type: ControlType.Number,
+      title: "X Min"
     },
-    y: {
-        y_outputMin: {
-            type: ControlType.Number,
-            title: "Y Min",
-        },
-        y_outputMax: {
-            type: ControlType.Number,
-            title: "Y Max",
-        },
+    x_outputMax: {
+      type: ControlType.Number,
+      title: "X Max"
+    }
+  },
+  y: {
+    y_outputMin: {
+      type: ControlType.Number,
+      title: "Y Min"
     },
-    opacity: {
-        opacity_outputMin: {
-            type: ControlType.Number,
-            min: 0,
-            max: 1,
-            title: "Opacity Min",
-        },
-        opacity_outputMax: {
-            type: ControlType.Number,
-            min: 0,
-            max: 1,
-            title: "Opacity Max",
-        },
+    y_outputMax: {
+      type: ControlType.Number,
+      title: "Y Max"
+    }
+  },
+  opacity: {
+    opacity_outputMin: {
+      type: ControlType.Number,
+      min: 0,
+      max: 1,
+      title: "Opacity Min"
     },
-    scale: {
-        scale_outputMin: {
-            type: ControlType.Number,
-            title: "Scale Min",
-        },
-        scale_outputMax: {
-            type: ControlType.Number,
-            title: "Scale Max",
-        },
+    opacity_outputMax: {
+      type: ControlType.Number,
+      min: 0,
+      max: 1,
+      title: "Opacity Max"
+    }
+  },
+  scale: {
+    scale_outputMin: {
+      type: ControlType.Number,
+      title: "Scale Min"
     },
-    rotate: {
-        rotate_outputMin: {
-            type: ControlType.Number,
-            min: 0,
-            max: 360,
-            title: "Rotate Min",
-        },
-        rotate_outputMax: {
-            type: ControlType.Number,
-            min: 0,
-            max: 360,
-            title: "Rotate Max",
-        },
+    scale_outputMax: {
+      type: ControlType.Number,
+      title: "Scale Max"
+    }
+  },
+  rotate: {
+    rotate_outputMin: {
+      type: ControlType.Number,
+      min: 0,
+      max: 360,
+      title: "Rotate Min"
     },
-    backgroundColor: {
-        backgroundColor_outputMin: {
-            type: ControlType.Color,
-            title: "BG Min",
-        },
-        backgroundColor_outputMax: {
-            type: ControlType.Color,
-            title: "BG Max",
-        },
+    rotate_outputMax: {
+      type: ControlType.Number,
+      min: 0,
+      max: 360,
+      title: "Rotate Max"
+    }
+  },
+  backgroundColor: {
+    backgroundColor_outputMin: {
+      type: ControlType.Color,
+      title: "BG Min"
     },
+    backgroundColor_outputMax: {
+      type: ControlType.Color,
+      title: "BG Max"
+    }
+  }
 }
 
 // 1. Add "hidden" functions
 // 2. Flatten properties into a shallow object
 function getOutputPropertyControls() {
-    return Object.keys(properties).reduce((result, propName) => {
-        const controlGroup = properties[propName]
-        // Flatten
-        const flattened = Object.keys(controlGroup).reduce(
-            (preValue, outputProp) => {
-                // add "hidden" function
-                const control = {
-                    ...controlGroup[outputProp],
-                    hidden: props => props.property !== propName,
-                }
-                return { ...preValue, [outputProp]: control }
-            },
-            {}
-        )
-        return { ...result, ...flattened }
-    }, {})
+  return Object.keys(properties).reduce((result, propName) => {
+    const controlGroup = properties[propName]
+    // Flatten
+    const flattened = Object.keys(controlGroup).reduce(
+      (preValue, outputProp) => {
+        // add "hidden" function
+        const control = {
+          ...controlGroup[outputProp],
+          hidden: props => props.property !== propName
+        }
+        return { ...preValue, [outputProp]: control }
+      },
+      {}
+    )
+    return { ...result, ...flattened }
+  }, {})
 }
 
 addPropertyControls(ParallaxLayer, {
-    children: {
-        type: ControlType.ComponentInstance,
-    },
-    dependsOn: {
-        type: ControlType.Enum,
-        options: ["x", "y"],
-        defaultValue: "y",
-    },
-    property: {
-        type: ControlType.Enum,
-        options: Object.keys(properties),
-        defaultValue: "rotate",
-    },
-    inputMin: {
-        type: ControlType.Number,
-        title: "Scroll Min",
-        min: 0,
-    },
-    inputMax: {
-        type: ControlType.Number,
-        title: "Scroll Max",
-        min: 0,
-    },
-    ...getOutputPropertyControls(),
+  children: {
+    type: ControlType.ComponentInstance,
+    title: "Content"
+  },
+  dependsOn: {
+    type: ControlType.Enum,
+    options: ["x", "y"],
+    defaultValue: "y"
+  },
+  property: {
+    type: ControlType.Enum,
+    options: Object.keys(properties),
+    defaultValue: "rotate"
+  },
+  inputMin: {
+    type: ControlType.Number,
+    title: "Scroll Min",
+    min: 0
+  },
+  inputMax: {
+    type: ControlType.Number,
+    title: "Scroll Max",
+    min: 0
+  },
+  ...getOutputPropertyControls()
 })
--- code/ParallaxScroll.tsx
+++ code/ParallaxScroll.tsx
@@ -1,27 +1,66 @@
 import * as React from "react"
-import { Scroll, useMotionValue } from "framer"
+import {
+  Scroll,
+  useMotionValue,
+  RenderTarget,
+  addPropertyControls,
+  ControlType
+} from "framer"
 import { ScrollContext } from "./ScrollContext"
 import { usePositiveOffset } from "use-parallax"
+import { EmptyConnector } from "./EmptyConnector"
 
-export function ParallaxScroll({ children, ...props }) {
-    const offsetX = useMotionValue(0)
-    const offsetY = useMotionValue(0)
-    const scrollX = usePositiveOffset(offsetX)
-    const scrollY = usePositiveOffset(offsetY)
-    return (
-        <ScrollContext.Provider value={{ scrollX, scrollY }}>
-            <Scroll
-                {...props}
-                contentOffsetX={offsetX}
-                contentOffsetY={offsetY}
-            >
-                {children}
-            </Scroll>
-        </ScrollContext.Provider>
-    )
+function Thumbnail() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="200"
+      height="200"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-book"
+      viewBox="0 0 24 24"
+    >
+      <path d="M4 19.5A2.5 2.5 0 016.5 17H20" />
+      <path d="M6.5 2H20v20H6.5A2.5 2.5 0 014 19.5v-15A2.5 2.5 0 016.5 2z" />
+    </svg>
+  )
+}
+
+function ParallaxScrollPreview({ children, ...props }) {
+  const offsetX = useMotionValue(0)
+  const offsetY = useMotionValue(0)
+  const scrollX = usePositiveOffset(offsetX)
+  const scrollY = usePositiveOffset(offsetY)
+  return React.Children.count(children) > 0 ? (
+    <ScrollContext.Provider value={{ scrollX, scrollY }}>
+      <Scroll {...props} contentOffsetX={offsetX} contentOffsetY={offsetY}>
+        {children}
+      </Scroll>
+    </ScrollContext.Provider>
+  ) : (
+    <EmptyConnector title="Connect to a Frame →" description="" />
+  )
+}
+
+export function ParallaxScroll(props) {
+  const Comp =
+    RenderTarget.current() === RenderTarget.thumbnail
+      ? Thumbnail
+      : ParallaxScrollPreview
+  return <Comp {...props} />
 }
 
 // Use Scroll["defaultProps"] instead of Scroll.defaultProps
 // to prevent TypeScript error
 ParallaxScroll.defaultProps = Scroll["defaultProps"]
-ParallaxScroll.propertyControls = Scroll["propertyControls"]
+addPropertyControls(ParallaxScroll, {
+  ...Scroll["propertyControls"],
+  children: {
+    type: ControlType.ComponentInstance,
+    title: "Content"
+  }
+})
