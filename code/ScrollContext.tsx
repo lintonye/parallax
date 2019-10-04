ef162decaf50552c6933f58cf44dd5dc52dd05be
Linton Ye <linton@jimulabs.com>
2019-10-04 15:33:33 -0700

    Fix override error in published package

--- code/ScrollContext.tsx
+++ code/ScrollContext.tsx
@@ -1,6 +1,14 @@
 import * as React from "react"
 
-export const ScrollContext = React.createContext({
-  scrollX: null,
-  scrollY: null
-})
+const key = "lintonye.parallax.ScrollContext"
+
+// Put the context object in window. This is needed to make sure the overrides work
+// in the published package.
+if (!window[key]) {
+  window[key] = React.createContext({
+    scrollX: null,
+    scrollY: null
+  })
+}
+
+export const ScrollContext = window[key]
